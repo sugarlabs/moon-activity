e78a1fe86addca3f6b5970e2f60a6622ddbe1792
Gary Martin <gary@garycmartin.com>
2008-03-16 17:50:12 +0000

    Improved setup.py to ignore git stuff for easier building

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ try:
 	bundlebuilder.start("moon")
 except ImportError:
 	import os
-	os.system("find ./ | sed 's,^./,Moon.activity/,g' > MANIFEST")
+	os.system("find ./ | sed 's,^./,Moon.activity/,g' | sed 's,//,/,g' | grep -v /.git > MANIFEST")
 	os.system('rm Moon.xo')
 	os.chdir('..')
-	os.system('zip -r Moon.xo Moon.activity')
+	os.system('zip -r Moon.xo Moon.activity -x \*/.git\*')
 	os.system('mv Moon.xo ./Moon.activity')
 	os.chdir('Moon.activity')
