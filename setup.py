4f68152110185c257dc5316f8a85d4c794c9f78c
James Cameron <quozl@laptop.org>
2018-01-12 13:22:40 +1100

    Remove exception handler in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python
-try:
-	from sugar.activity import bundlebuilder
-	bundlebuilder.start()
-except ImportError:
-	print "Error: sugar.activity.Bundlebuilder not found."
+from sugar.activity import bundlebuilder
+bundlebuilder.start()
